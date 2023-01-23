01e8a544d07022a7019f0ee87a881475460ab1d0
niku153 <104482078+niku153@users.noreply.github.com>
2023-01-23 11:00:04 +0800

    Added password field to serializer
    Changed Serializer to ModelSerializer and added 'fields' - this is required when using ModelSerializer.

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 from .models import CustomUser
 
-class CustomUserSerializer(serializers.Serializer):
-    id = serializers.ReadOnlyField()
-    username = serializers.CharField(max_length=200)
-    email = serializers.CharField(max_length=200)
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id','email', 'username', 'password']
+        id = serializers.ReadOnlyField()
+        username = serializers.CharField(max_length=200)
+        email = serializers.CharField(max_length=200)
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        return CustomUser.objects.create(**validated_data)
+        user = CustomUser(
+            email=validated_data['email'],
+            username = validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
